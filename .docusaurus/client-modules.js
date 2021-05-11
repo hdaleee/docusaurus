c79f4b02bd0dbb70169513295c95911045e85fb8
문상현 <hdaleee@munsanghyeon-ui-MacBookPro.local>
2021-05-11 09:20:57 +0900

    test

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/hdaleee/publ/publ-tech-blog/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/hdaleee/publ/publ-tech-blog/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/hdaleee/publ/publ-tech-blog/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/hdaleee/publ/publ-tech-blog/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/hdaleee/publ/publ-tech-blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/hdaleee/publ/publ-tech-blog/src/css/custom.css"),
+];
---
+++ .docusaurus/routes.js
@@ -0,0 +1,126 @@
+
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+export default [
+{
+  path: '/',
+  component: ComponentCreator('/','deb'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug',
+  component: ComponentCreator('/__docusaurus/debug','3d6'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/config',
+  component: ComponentCreator('/__docusaurus/debug/config','914'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/content',
+  component: ComponentCreator('/__docusaurus/debug/content','c28'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/globalData',
+  component: ComponentCreator('/__docusaurus/debug/globalData','3cf'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/metadata',
+  component: ComponentCreator('/__docusaurus/debug/metadata','31b'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/registry',
+  component: ComponentCreator('/__docusaurus/debug/registry','0da'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/routes',
+  component: ComponentCreator('/__docusaurus/debug/routes','244'),
+  exact: true,
+},
+{
+  path: '/blog',
+  component: ComponentCreator('/blog','28e'),
+  exact: true,
+},
+{
+  path: '/blog/hello-world',
+  component: ComponentCreator('/blog/hello-world','ae4'),
+  exact: true,
+},
+{
+  path: '/blog/hola',
+  component: ComponentCreator('/blog/hola','603'),
+  exact: true,
+},
+{
+  path: '/blog/tags',
+  component: ComponentCreator('/blog/tags','899'),
+  exact: true,
+},
+{
+  path: '/blog/tags/docusaurus',
+  component: ComponentCreator('/blog/tags/docusaurus','a14'),
+  exact: true,
+},
+{
+  path: '/blog/tags/facebook',
+  component: ComponentCreator('/blog/tags/facebook','160'),
+  exact: true,
+},
+{
+  path: '/blog/tags/hello',
+  component: ComponentCreator('/blog/tags/hello','430'),
+  exact: true,
+},
+{
+  path: '/blog/tags/hola',
+  component: ComponentCreator('/blog/tags/hola','9d1'),
+  exact: true,
+},
+{
+  path: '/blog/welcome',
+  component: ComponentCreator('/blog/welcome','749'),
+  exact: true,
+},
+{
+  path: '/markdown-page',
+  component: ComponentCreator('/markdown-page','be1'),
+  exact: true,
+},
+{
+  path: '/docs',
+  component: ComponentCreator('/docs','512'),
+  
+  routes: [
+{
+  path: '/docs/',
+  component: ComponentCreator('/docs/','87d'),
+  exact: true,
+},
+{
+  path: '/docs/doc2',
+  component: ComponentCreator('/docs/doc2','fd3'),
+  exact: true,
+},
+{
+  path: '/docs/doc3',
+  component: ComponentCreator('/docs/doc3','e02'),
+  exact: true,
+},
+{
+  path: '/docs/mdx',
+  component: ComponentCreator('/docs/mdx','398'),
+  exact: true,
+},
+]
+},
+{
+  path: '*',
+  component: ComponentCreator('*')
+}
+];
